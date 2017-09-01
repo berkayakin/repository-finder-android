141427d8bbeedf5e2f2466bd5f2cb05a877e4d65
Berkay AKIN <berkayakin@users.noreply.github.com>
2017-09-01 19:06:46 +0200

    Keyboard search button action has been added to the keyboard of the search field

--- app/src/main/java/repositoryfinder/com/testchambr/repositoryfinder/activities/RepositoriesActivity.java
+++ app/src/main/java/repositoryfinder/com/testchambr/repositoryfinder/activities/RepositoriesActivity.java
@@ -5,10 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.BindView;
@@ -60,6 +63,14 @@ public class RepositoriesActivity extends AppCompatActivity implements Repositor
         };
 
         repositoriesRecyclerView.addOnScrollListener(mScrollListener);
+
+        searchEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                searchButtonAction();
+                return false;
+            }
+        });
 
         if(savedInstanceState != null) {
             currentQuery = savedInstanceState.getString("currentQuery");
